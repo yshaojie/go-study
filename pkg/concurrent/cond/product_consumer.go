b998e9a64e111d97861c294dd985c90e8937a008
shaojieyue <4yxy1986@gmail.com>
2023-09-23 11:11:22 +0800

    11

--- pkg/concurrent/cond/product_consumer.go
+++ pkg/concurrent/cond/product_consumer.go
@@ -8,21 +8,22 @@ import (
 )
 
 func main() {
+	locker := &sync.Mutex{}
 	queue := concurrent.Queue{
-		Capacity:     100,
+		Capacity:     4,
 		Items:        nil,
-		ProductCond:  sync.NewCond(&sync.Mutex{}),
-		ConsumerCond: sync.NewCond(&sync.Mutex{}),
+		ProductCond:  sync.NewCond(locker),
+		ConsumerCond: sync.NewCond(locker),
 	}
 	wg := sync.WaitGroup{}
 	count := 0
 	mutex := sync.Mutex{}
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 5; i++ {
 		go func() {
 			wg.Add(1)
 			time.Sleep(time.Second * 3)
 			defer wg.Done()
-			for i := 0; i < 10000; i++ {
+			for i := 0; i < 20000; i++ {
 				mutex.Lock()
 				count = count + 1
 				mutex.Unlock()
@@ -35,7 +36,7 @@ func main() {
 		go func() {
 			wg.Add(1)
 			defer wg.Done()
-			for i := 0; i < 1000; i++ {
+			for i := 0; i < 10000; i++ {
 				queue.Push(rand.Int())
 			}
 		}()
--- pkg/concurrent/queue.go
+++ pkg/concurrent/queue.go
@@ -10,11 +10,11 @@ type Queue struct {
 }
 
 func (q *Queue) Push(item int) {
-	//q.ProductCond.L.Lock()
-	//defer q.ProductCond.L.Unlock()
-	//if len(q.Items) >= q.Capacity {
-	//	q.ProductCond.Wait()
-	//}
+	q.ProductCond.L.Lock()
+	defer q.ProductCond.L.Unlock()
+	for len(q.Items) >= q.Capacity {
+		q.ProductCond.Wait()
+	}
 	q.Items = append(q.Items, item)
 	q.ConsumerCond.Signal()
 }
@@ -22,13 +22,11 @@ func (q *Queue) Push(item int) {
 func (q *Queue) Pop() int {
 	q.ConsumerCond.L.Lock()
 	defer q.ConsumerCond.L.Unlock()
-	println(len(q.Items))
-	if len(q.Items) <= 0 {
-		println("a")
+	for len(q.Items) <= 0 {
 		q.ConsumerCond.Wait()
 	}
 	item := q.Items[0]
-	q.Items = q.Items[:1]
-	//q.ProductCond.Signal()
+	q.Items = q.Items[1:]
+	q.ProductCond.Signal()
 	return item
 }
